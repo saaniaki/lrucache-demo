66ba05ab0f31115c6b73f28d718c1bba7687798d
Ali Niaki <ali.n@brainfx.com>
2021-09-06 19:31:26 -0400

    Installs the NestJS Axios, Class Transformer and Validator libraries.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LruCacheService } from './lru-cache/lru-cache.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [],
+  imports: [HttpModule],
   controllers: [AppController],
   providers: [AppService, LruCacheService],
 })
